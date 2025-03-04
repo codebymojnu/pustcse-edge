9084abe94de74f05502bdb06017ec4bb7cd9854d
codebymojnu <thisismojnu@gmail.com>
2025-03-05 05:26:34 +0600

    pustcse-edge

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import styles from "./Logo.module.css";
 const Logo = () => {
   return (
     <div className={styles.logo_container}>
-      <span className={styles.logo_first_letter}>The Chapters of Mojnu</span>
+      <span className={styles.logo_first_letter}>EDGE PUSTCSE</span>
     </div>
   );
 };
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,29 +1,27 @@
 import { useRouter } from "next/router";
 import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
-import Logo from "./components/Logo"
-
+import Logo from "./components/Logo";
 
 const config: DocsThemeConfig = {
   logo: <Logo />,
   project: {
-    link: "https://github.com/codebymojnu/the-chapters-of-mojnu",
+    link: "https://github.com/codebymojnu/pustcse-edge",
   },
   chat: {
     link: "https://discord.com/mojnu13",
   },
-  docsRepositoryBase:
-    "https://github.com/codebymojnu/the-chapters-of-mojnu/blob/main",
+  docsRepositoryBase: "https://github.com/codebymojnu/pustcse-edge/blob/main",
   footer: {
-    text: "The Chapters of Mojnu",
+    text: "PUSTCSE EDGE Project",
   },
   useNextSeoProps() {
     const { route } = useRouter();
     if (route !== "/") {
       return {
-        titleTemplate: "%s – The Chaptes of Mojnu",
+        titleTemplate: "%s – PUSTCSE EDGE Project",
       };
     }
-    return { titleTemplate: "The Chaptes of Mojnu" };
+    return { titleTemplate: "PUSTCSE EDGE Project" };
   },
   head: () => {
     const { asPath } = useRouter();
@@ -34,29 +32,23 @@ const config: DocsThemeConfig = {
       <>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <link rel="canonical" href="https://the-chapters-of-mojnu.vercel.app" />
+        <link rel="canonical" href="https://pustcse-edge.vercel.app" />
         <meta
           property="og:url"
-          content={`https://the-chapters-of-mojnu.vercel.app${asPath}`}
+          content={`https://pustcse-edge.vercel.app${asPath}`}
         />
-        <meta property="og:site_name" content="The Chapters of Mojnu" />
-        <meta name="og:title" content="The Chapters of Mojnu" />
+        <meta property="og:site_name" content="PUSTCSE EDGE" />
+        <meta name="og:title" content="PUSTCSE EDGE" />
         <meta httpEquiv="Content-Language" content="bn" />
         <meta name="viewport" content="width=device-width" />
-        <meta name="keywords" content="The Chapters of Mojnu" />
+        <meta name="keywords" content="PUSTCSE EDGE" />
         <meta
           property="description"
-          content={
-            meta.description ||
-            "The Chapters of Mojnu:  আমার জীবনের স্বপ্ন, লক্ষ্য, সফলতা, ব্যর্থতা, এবং ভুলগুলো কাটিয়ে উঠার গল্প নিয়ে এই ডকুমেন্টেশন।"
-          }
+          content={meta.description || "PUSTCSE EDGE Project"}
         />
         <meta
           property="og:description"
-          content={
-            meta.description ||
-            "The Chapters of Mojnu:  আমার জীবনের স্বপ্ন, লক্ষ্য, সফলতা, ব্যর্থতা, এবং ভুলগুলো কাটিয়ে উঠার গল্প নিয়ে এই ডকুমেন্টেশন।"
-          }
+          content={meta.description || "PUSTCSE EDGE Project"}
         />
       </>
     );
